40c39ef7566b8f601b5360a77bc35a7975bb8249
Nickys0 <nicholasscardino07@gmail.com>
2025-08-25 22:54:00 +0200

    Using nob.h to testing the library

---
+++ nob.c
@@ -0,0 +1,101 @@
+
+#define NOB_IMPLEMENTATION
+#define NOB_STRIP_PREFIX
+#define NOB_EXPERIMENTAL_DELETE_OLD
+#define NOB_WARN_DEPRECATED
+#include "nob.h"
+#undef rename                   // Testing for backward compatibility after v1.20.6
+
+#define BUILD_FOLDER ".build/"
+#define TESTS_FOLDER "tests/"
+
+const char *test_names[] = {
+    "basic_flags",
+    "commands"
+};
+
+#define test_names_count ARRAY_LEN(test_names)
+
+const char* basic_flags_args[] = {
+    "--bool", "--i32", "32", "-s", "Static", "-d", "Dynamic", NULL
+};
+
+const char* command_args[] = {
+    "new", "new_value", NULL
+};
+
+const char** test_args[test_names_count] = {
+    basic_flags_args,
+    command_args
+};
+
+bool build_and_run_test(Cmd *cmd, const char *test_name, const char** args){
+    size_t mark = temp_save();
+
+    const char *bin_path = temp_sprintf("%s%s", BUILD_FOLDER TESTS_FOLDER, test_name);
+    const char *src_path = temp_sprintf("%s%s.c", TESTS_FOLDER, test_name);
+
+    nob_cc(cmd);
+    nob_cc_flags(cmd);
+    nob_cmd_append(cmd, "-I.");
+    nob_cc_output(cmd, bin_path);
+    nob_cc_inputs(cmd, src_path);
+
+    if (!cmd_run(cmd)) return false;
+
+    cmd_append(cmd, temp_sprintf("./"BUILD_FOLDER TESTS_FOLDER"%s", test_name));
+    for(size_t i = 0; ; ++i){
+        const char* arg = args[i];
+        if(arg == NULL) break;
+        cmd_append(cmd, arg);
+    }
+    if(!cmd_run(cmd))    return false;
+
+    nob_log(INFO, "--- %s finished ---", bin_path);
+
+    temp_rewind(mark);
+    return true;
+}
+
+int main(int argc, char **argv){
+    NOB_GO_REBUILD_URSELF_PLUS(argc, argv, "nob.h");
+
+    Cmd cmd = {0};
+
+    const char *program_name = shift(argv, argc);
+    const char *command_name = "test";
+    if(argc > 0) command_name = shift(argv, argc);
+
+    if(!mkdir_if_not_exists(BUILD_FOLDER)) return 1;
+    if(!mkdir_if_not_exists(BUILD_FOLDER TESTS_FOLDER)) return 1;
+
+    if(strcmp(command_name, "test") == 0) {
+        if(argc <= 0) {
+            for (size_t i = 0; i < test_names_count; ++i) {
+                if(!build_and_run_test(&cmd, test_names[i], test_args[i])) return 1;
+            }
+            return 0;
+        }
+        
+        size_t i = 0;
+
+        while(argc > 0) {
+            const char *test_name = shift(argv, argc);
+            if (!build_and_run_test(&cmd, test_name, test_args[i++])) return 1;
+        }
+
+        return 0;
+    }
+
+    if(strcmp(command_name, "list") == 0){
+        nob_log(INFO, "Tests:");
+        for (size_t i = 0; i < test_names_count; ++i) {
+            nob_log(INFO, "    %s", test_names[i]);
+        }
+        nob_log(INFO, "Use %s test <names...> to run individual tests", program_name);
+        return 0;
+    }
+
+    nob_log(ERROR, "Unknown command %s", command_name);
+    return 1;
+}
